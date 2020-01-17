52bd456cfba1801362bb048d1cea3d9e09d90d18
Jadan Morrow <jadanmorrow@outlookcom>
2020-01-17 08:51:39 -0700

    first commit

---
+++ Module-6/javascript.js
@@ -0,0 +1,33 @@
+document.getElementById("Button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "350px";
+
+});
+
+document.getElementById("Button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "Blue" 
+
+});
+
+document.getElementById("Button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "10%";
+
+});
+
+document.getElementById("Button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+    document.getElementById("box").style.opacity = "100%"
+
+    document.getElementById("box").style.backgroundColor = "Orange"
+
+});
+
+
+
+
+
+
